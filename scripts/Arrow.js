259491e333d3ed96a7d14728e603d033203036ff
Eugene <Eugene@192.168.0.102>
2013-04-24 09:43:24 +0300

    Editor updated.

--- scripts/Arrow.js
+++ scripts/Arrow.js
@@ -1,10 +1,13 @@
 var OSMEX = OSMEX || { REVISION: '1' };
 
-OSMEX.Arrow = function ( dir, origin, length, hex ) {
+OSMEX.Arrow = function ( dir, origin, length, hex, type ) {
     
     THREE.Object3D.call( this );
     this.name = "Arrow";
-	
+    
+    this.arrowContainer = new THREE.Object3D();
+    this.add(this.arrowContainer);
+    
     this.pickable = false;
     
     this.dir = null;
@@ -18,29 +21,40 @@ OSMEX.Arrow = function ( dir, origin, length, hex ) {
     lineGeometry.vertices.push( new THREE.Vector3( 0, 1, 0 ) );
 
     this.line = new THREE.Line( lineGeometry, new THREE.LineBasicMaterial( {color: hex} ) );
-    this.add( this.line );
+    this.arrowContainer.add( this.line );
 
     var meshMaterial = new THREE.MeshPhongMaterial( {
         color: hex, 
         shading: THREE.SmoothShading, 
         ambient: 0xffffff
-    } );//new THREE.MeshBasicMaterial( { color: hex } );
+    } );
+    this.type = type;
     
-    /*var cylinderGeometry = new THREE.CylinderGeometry( 0.025, 0.025, 1, 5, 5 );
-    this.cylinder = new THREE.Mesh( cylinderGeometry, meshMaterial );
-    this.cylinder.position.set( 0, 0.5, 0 );
-    this.cylinder.pickable = false;
-    this.add( this.cylinder );*/
+    if (this.type == "moving"){
+        var coneGeometry = new THREE.CylinderGeometry( 0, 1.5, 7.5, 5, 1 );
+        this.cone = new THREE.Mesh( coneGeometry, meshMaterial );
+        this.cone.position.set( 0, 30, 0 );
+        this.arrowContainer.add( this.cone );
+      /* var planeGeometry = new THREE.PlaneGeometry(8,8,8,8);
+        this.plane = new THREE.Mesh( planeGeometry, meshMaterial );
+        this.plane.position.set( 0, 4, 0 );
+        this.plane.rotation.set( 1.5, 1.5, 0);
+        this.add( this.plane );*/
+        
+    }else if (this.type == "sizing"){
+        var coneGeometry = new THREE.CylinderGeometry( 0, 1.5, 7.5, 5, 1 );
+        this.cone = new THREE.Mesh( coneGeometry, meshMaterial );
+        this.cone.position.set( 0, 30, 0 );
+        this.arrowContainer.add( this.cone );
+     }
 
-    var coneGeometry = new THREE.CylinderGeometry( 0, 1.5, 7.5, 5, 1 );
-    this.cone = new THREE.Mesh( coneGeometry, meshMaterial );
-    this.cone.position.set( 0, 1, 0 );
-    this.add( this.cone );
-
+    
     if ( origin instanceof THREE.Vector3 ) this.position = origin;
 	
     this.len = 0;
+    this.defaultLength = length;
     this.setLength( length );
+ 
 };
 
 OSMEX.Arrow.prototype = Object.create( THREE.Object3D.prototype );
@@ -61,27 +75,31 @@ OSMEX.Arrow.prototype.setDirection = function ( dir ) {
     }
     else
     {
-        axis = upVector.crossSelf( this.dir );
+        axis = new THREE.Vector3().crossVectors( upVector, this.dir );
     }
 	
     var radians = Math.acos( cosa );
 	
-    this.matrix = new THREE.Matrix4().makeRotationAxis( axis, radians );
-    this.rotation.setEulerFromRotationMatrix( this.matrix, this.eulerOrder );
+    this.arrowContainer.matrix = new THREE.Matrix4().makeRotationAxis( axis, radians );
+    this.arrowContainer.rotation.setEulerFromRotationMatrix( this.arrowContainer.matrix, this.arrowContainer.eulerOrder );
 };
 
 OSMEX.Arrow.prototype.setLength = function ( length ) {
     
     this.len = length;
     this.line.scale.y = length;
-    this.cone.position.y = length;
+    if (this.type == "sizing")this.cone.position.y = length; 
     
-    //this.scale.set( length, length, length );
+};
+
+OSMEX.Arrow.prototype.restoreDefaultLength = function ( ) {
+    
+    this.setLength(this.defaultLength);
 };
 
 OSMEX.Arrow.prototype.setColor = function ( hex ) {
     
     this.line.material.color.setHex( hex );
-    //this.cylinder.material.color.setHex( hex );
     this.cone.material.color.setHex( hex );
+   // if (this.type == "moving") this.plane.material.color.setHex( hex );
 }; 
