4d6e9b351751914d17c988920010fef6db514445
Jemma <Jemma@JW>
2013-04-28 11:37:21 +0300

    Added new csg operations icons.

--- scripts/AjaxRequests.js
+++ scripts/AjaxRequests.js
@@ -20,20 +20,20 @@ function ajaxPostScene(array) {
             cache: false,
             data: {object_uid: array[i].id, object_scaleX: array[i].scale.x, object_scaleY: array[i].scale.y, object_scaleZ: array[i].scale.z, object_rotationX: array[i].rotation.x, object_rotationY: array[i].rotation.y, object_rotationZ: array[i].rotation.z, object_positionLat: coords.lat, object_positionLon: coords.lon,object_positionHeight:array[i].position.y, object_referID: array[i].typeID, isDeleted: array[i].isDeleted},
             success: function(data) {
-                alert(data);
+                //alert(data);
             }
         })
     }
 }
 
-function ajaxNewSketch(name, category, serializedGeometry) {
+function ajaxNewSketch(name, category, serializedGeometry, imageData) {
     $.ajax({
         type: "POST",
         url: "server_scripts/NewSketch.php",
         cache: false,
-        data: {name: name.val(), category: category.val(), geometry: serializedGeometry}, 
+        data: {name: name.val(), category: category.val(), geometry: serializedGeometry, imageData: imageData}, 
         success: function(data) {
-            alert(data);
+            //alert(data);
         }
     });
 }
