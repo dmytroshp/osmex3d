51bb34cc0ddd4ef8514d262e8ce7d49b3060b192
Gleab <Gleab@rgenotebook>
2013-04-29 11:29:59 +0300

    Sketch builder integration init

--- index.php
+++ index.php
@@ -54,8 +54,37 @@ $mlon=(isset($_GET['mlon'])&& is_numeric($_GET['mlon']))?$_GET['mlon']:0;
         <script type="text/javascript" src="jquery/jquery.color.js"></script>
         <script type="text/javascript" src="jquery/jquery.Jcrop.min.js"></script>
         
+        <script src="threejs/three.js"></script>
+        <script src="threejs/GeometryExporter.js"></script>
+        <script src="threejs/ThreeCSG.js"></script>
+        
+        <script src="scripts/BoundingBox.js"></script>
+        <script src="scripts/ObjectScene.js"></script>
+        <script src="scripts/InterfaceScene.js"></script>
+        <script src="scripts/Grid.js"></script>
+        <script src="scripts/Camera.js"></script>
+        <script src="scripts/CameraController.js"></script>
+        <script src="scripts/Block.js"></script>
+        <script src="scripts/Cube.js"></script>
+        <script src="scripts/ScaleCube.js"></script>
+        <script src="scripts/Arrow.js"></script>
+        <script src="scripts/SizerArrow.js"></script>
+        <script src="scripts/SizerGizmo.js"></script>
+        <script src="scripts/MovingGizmo.js"></script>
+        <script src="scripts/MovingGizmoPlane.js"></script>
+        <script src="scripts/MovingArrow.js"></script>
+        <script src="scripts/Torus.js"></script>
+        <script src="scripts/RotationTorus.js"></script>
+        <script src="scripts/RotationGizmoOverlay.js"></script>
+        <script src="scripts/RotationGizmo.js"></script>
+        <script src="scripts/BoxBuilder.js"></script>
+        <script src="scripts/SketchFactory.js"></script>
+
+        <script src="scripts/AjaxRequests.js"></script>
+        
         <script type="text/javascript" src="scripts/TextureBuilder.prototypes.js"></script>
         <script type="text/javascript" src="scripts/TextureBuilder.js"></script>
+        <script type="text/javascript" src="scripts/SketchBuilder.js"></script>
         
         <link type="text/css" href="css/smoothness/jquery-ui-1.10.2.custom.min.css" rel="stylesheet" />
         <link type="text/css" href="css/jcrop/jquery.Jcrop.min.css" rel="stylesheet" />
@@ -137,7 +166,7 @@ HERE;
                     },
                     tabSketch:{
                         url:'ajax/sketchBuilder.html',
-                        activator:function(){}
+                        activator:function(){prepareSketchBuilder();}
                     },
                     tabTxt:{
                         url:'ajax/textureBuilder.html',
