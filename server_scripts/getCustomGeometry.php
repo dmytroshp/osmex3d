01e9ba02b9a70225417ada0f97c505359decfd82
Eugenald <knowing@ukr.net>
2013-04-30 21:24:25 +0300

    Fixed script.

--- server_scripts/getCustomGeometry.php
+++ server_scripts/getCustomGeometry.php
@@ -1,14 +1,13 @@
 <?php
 header('Content-Type: application/json; utf-8');
 require_once("config.php");
-$rID = $_POST['id'];
+$rID = $_GET['id'];
 $query =  sprintf("SELECT geometryStr FROM objecttype WHERE id=".$rID."");
 $result = mysql_query($query);
 $str = mysql_fetch_array($result);
 mysql_free_result($result);
 $geometry = $str['geometryStr'];
 $response = unserialize($geometry);
-mysql_close($link);
 echo json_encode($response);
 mysql_close($connection);
 ?>
