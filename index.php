8990ee9f5b66dcae6b34ae3aea99cec253e2f026
Maksym <gsmaksss@gmail.com>
2013-05-02 20:46:03 +0300

    changed logo size

--- index.php
+++ index.php
@@ -471,7 +471,7 @@ HERE;
         <body>
             <div id="mainContainer">
                 <div id="sidebar">
-		    <div id="logo"><img src="img/logo.png" height="130" width="437"></div>
+		    <div id="logo"><p align="center"><img src="img/logo_new.png" height="87" width="265"></p></div>
                     <div id="searchDivc">
                     <div id="osmSearch">
                         <form id="osmSearchForm">
